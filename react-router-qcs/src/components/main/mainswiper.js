5891eddc08c164924895ba63dcff2677ec8f0916
yinyinyinyinyin <348029293@qq.com>
2020-03-27 14:57:53 +0800

    aa

--- react-router-qcs/src/components/main/mainswiper.js
+++ react-router-qcs/src/components/main/mainswiper.js
@@ -1,4 +1,8 @@
 import React,{Component} from 'react';
+
+//引入语法检查的组件
+import PropTypes from 'prop-types';
+
 //轮播 3.4.2版本的引入
 import Swiper from 'swiper/dist/js/swiper.min.js';
 import 'swiper/dist/css/swiper.min.css';
@@ -40,4 +44,13 @@ class MainSwiper extends Component{
 		
 	}
 }
+
+//进行数据的格式检查,优势是:当父组件传过来的数据格式不正确时,会有红色提示
+//array  传过来的是个数组
+//isRequired 传过来ide值不允许是空
+//https://www.jianshu.com/p/a73fb26c88b5
+MainSwiper.propTypes = {
+	lunboList:PropTypes.array.isRequired
+}
+
 export default MainSwiper;
