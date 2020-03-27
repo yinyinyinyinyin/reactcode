196c0b3c0f9c96ea007c1609f711282e221b2cdd
yinyinyinyinyin <348029293@qq.com>
2020-03-27 11:07:46 +0800

    aa

--- react-router-qcs/src/pages/item/index.js
+++ react-router-qcs/src/pages/item/index.js
@@ -1,8 +1,14 @@
 import React,{Component} from 'react';
 import {HomeOutlined,ShoppingCartOutlined} from '@ant-design/icons';
-import {message} from 'antd';
+import {message,Badge} from 'antd';
 import './index.scss';
 class Item extends Component{
+	constructor(){
+		super();
+		this.state = {
+			countTotal : 0//购物车的总数量
+		}
+	}
 	//回到首页
 	goHome=()=>{
 		this.props.history.push('/');
@@ -30,9 +36,11 @@ class Item extends Component{
 				if(item.id === itemObj.item_id){//在购物车找到了该商品
 					//数量加一
 					item.num++;
+					item.checkType = true;
 					type = false;
 				}
 				data.push(item);
+				return null;
 			})
 		}
 		//新增数据
@@ -42,12 +50,40 @@ class Item extends Component{
 				num:1,
 				price:itemObj.min_price,
 				image_url:itemObj.over_image_url,
-				name:itemObj.item_short_name
+				name:itemObj.item_short_name,
+				checkType:true//商品添加购物车时,默认是选中状态
 			})
 		}
 		//将数据保存到缓存,在缓存中需要将数据保存成字符串
 		localStorage.setItem('cart',JSON.stringify(data)  );
 		message.info('添加成功',1);
+		
+		
+		//计算购物车中的总数量
+		this.totalNum();
+	}
+	//计算购物车中的商品总数
+	totalNum = ()=>{
+		//获取缓存中的数据
+		let arr = JSON.parse(localStorage.getItem('cart'));
+		let total = 0;
+		if(arr !== null && arr.length){//购物车中有商品
+			arr.map(item=>{
+				//计算了总数量
+				total += item.num;
+				return null;
+			})
+		}
+		console.log(73,total);
+		//将总数量添加到 state
+		this.setState({
+			countTotal:total
+		})
+		
+	}
+	componentDidMount(){
+		//调用计算总数量的函数
+		this.totalNum();
 	}
 	
 	render(){
@@ -64,8 +100,8 @@ class Item extends Component{
 				<div>立即购买</div>
 			</div>
 			<div className="icon-btn">
-				<HomeOutlined onClick={this.goHome} />
-				<ShoppingCartOutlined onClick={this.goCart}/>
+				<HomeOutlined onClick={this.goHome} className="icon-home" />
+				<Badge count={this.state.countTotal} className="icon-cart"><ShoppingCartOutlined onClick={this.goCart}/></Badge>
 			</div>
 		</div>)
 	}
