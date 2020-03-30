0686ce08a8d40d0f5b4b5207d02f412070ca2ed5
yinyinyinyinyin <348029293@qq.com>
2020-03-30 11:55:28 +0800

    sss

--- react-router-qcs/src/pages/center/index.js
+++ react-router-qcs/src/pages/center/index.js
@@ -3,49 +3,17 @@ import {Redirect} from 'react-router-dom';
 import axios from 'axios';
 import {Button} from 'antd';
 import './index.scss';
-class Center extends Component{
-	constructor() {
-	    super();
-		this.state = {
-			tokenType:1//默认值1 表示成功
-		}
-	}
-	//调用后台接口判断登录状态
-	//token 叫做令牌,一般情况下是为了后台验证登录的状态,
-	//登录成功时会给前台发送一个token,后面每次请求数据时请带着这个token,
-	//如果token的验证和后台保存的一致,表示状态ok,就可以给前台提供数据了
-	componentDidMount(){
-		axios.get("http://127.0.0.1:7001/center",{
-			headers:{
-				'Authorization':localStorage['token']
-			}
-		}).then(res=>{
-			console.log(res);
-			if(res.data.code === 0){//登录成功的
-				this.setState({
-					tokenType:true
-				})
-			}else{//失败
-				this.setState({
-					tokenType:false
-				})
-			}
-		});
-	}
+ class Center extends Component{
 	logout=()=>{
 		localStorage['token'] = "";
 		this.props.history.push("/");
 	}
 	render(){
-		const {tokenType} = this.state;	
-		if(tokenType){//已经登录了
-			return <div className="center-con">
-			<h3>用户中心页</h3>
-			<Button type="primary" onClick={this.logout}>退出</Button>
+		return <div className="center-con">
+				<h3>用户中心页</h3>
+				<Button type="primary" onClick={this.logout}>退出</Button>
 			</div>
-		}else{//没有登录  ,跳转到登录页
-			return <Redirect to = "/login" />
-		}
+		
 	}
 }
 export default Center ;
--- react-router-qcs/src/router/index.js
+++ react-router-qcs/src/router/index.js
@@ -2,6 +2,11 @@
 //引入插件
 import React from 'react';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
+//引入需要配置的页面的config
+import {routerConfig} from './config';
+//引入守卫的组件
+import Auth from '../components/common/auth'; 
 //引入页面
 import Main from '../pages/main';
 import Global from '../pages/global';
@@ -10,8 +15,8 @@ import Mask from '../pages/mask';
 import Page404 from '../pages/page404';
 import List from '../pages/list';
 import Item from '../pages/item';
-import Cart from '../pages/cart';
-import Center from '../pages/center';
+//import Cart from '../pages/cart';
+//import Center from '../pages/center';
 import Login from '../pages/login';
 //引入 头部组件
 import Header from '../components/common/header';
@@ -30,9 +35,8 @@ import Header from '../components/common/header';
 					<Route path="/global" component={Global} />
 					<Route path="/list" component={List} />
 					<Route path="/item" component={Item} />
-					<Route path="/cart" component={Cart} />
 					<Route path="/login" component={Login}/>
-					<Route path="/center" component={Center} />
+					<Auth config={routerConfig} />
 					<Route  component={Page404} />
 				</Switch>
 			</div>
